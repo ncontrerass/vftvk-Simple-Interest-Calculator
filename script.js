abcc4155464ff07b8940e9f4dda63b579393c1ae
ncontrerass <50805848+ncontrerass@users.noreply.github.com>
2021-07-29 22:52:18 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 //compute function - Calculates result and displays it on the html
 function compute()
 {
-    validatePrincipal();
+    val r = validatePrincipal();
     
-    var principal = document.getElementById("principal").value;
+    if(r === true){
+        var principal = document.getElementById("principal").value;
 
-    var rate = document.getElementById("rate").value;
+        var rate = document.getElementById("rate").value;
 
-    var years = document.getElementById("years").value;
+        var years = document.getElementById("years").value;
 
-    var interest = principal * years * rate /100;
+        var interest = principal * years * rate /100;
 
-    var year = new Date().getFullYear()+parseInt(years);
+        var year = new Date().getFullYear()+parseInt(years);
 
-    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>";
-
+        document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>";
+    }
 }
 
 //updateRate function - reads and displays the slider value

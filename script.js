92b2a7671a11bbef9ab4dd3de6c67e08a91aee09
ncontrerass <50805848+ncontrerass@users.noreply.github.com>
2021-07-29 22:33:01 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,36 @@
+//compute function - Calculates result and displays it on the html
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+
+    var rate = document.getElementById("rate").value;
+
+    var years = document.getElementById("years").value;
+
+    var interest = principal * years * rate /100;
+
+    var year = new Date().getFullYear()+parseInt(years);
+
+    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>";
+
+}
+
+//updateRate function - reads and displays the slider value
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
+
+//validatePrincipal function - validates if the number is less or equals to zero, and shows an alert
+function validatePrincipal() 
+{
+    var principal = document.getElementById("principal").value;
+
+    if(principal <=0){
+        alert("Enter a positive number");
+        principal.focus();
+        return false;
+    }
 }
         
